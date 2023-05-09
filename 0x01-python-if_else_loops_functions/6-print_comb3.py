a013aec7b775a05f4520ea76755510a6bea66019
GHMatrix <gsmensah@yahoo.com>
2023-05-09 09:55:40 -0700

    Python scripts different combinations of 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        if i == 0 and j == 1:
-            print("01", end="")
-        else:
-            print("{:d}{:d}".format(i, j), end="")
-        if i == 8 and j == 9:
-            print()
-        else:
-            print(", ", end="")
+for a in range(0, 9):
+    for b in range(a + 1, 10):
+        print("{}{}".format(a, b), end=", ")
+print("89")
