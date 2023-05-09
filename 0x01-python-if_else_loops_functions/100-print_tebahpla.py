48a90f4712e870cf58894f40f9fef7ca22ba7997
GHMatrix <gsmensah@yahoo.com>
2023-05-09 14:29:32 -0700

    Reversed alphabets, alternating caps

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a')-1, -1):
-    if (ord('z')-c) % 2 == 0:
-        print(f"{chr(c)}", end="")
-    else:
-        print(f"{chr(c-32)}", end="")
+i = 0
+output = ""
+for c in range(ord('z'), ord('a') - 1, -1):
+    output += "{}".format(chr(c - i))
+    i = 32 if i == 0 else 0
+
+print(output)
