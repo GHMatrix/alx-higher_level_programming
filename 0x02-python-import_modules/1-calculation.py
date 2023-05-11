9312b4d1629b6edd0160fe2af9db17ca1f6e7a15
GHMatrix <gsmensah@yahoo.com>
2023-05-11 06:46:58 -0700

    python funtion to do some maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    operations = [
+        ("+", add),
+        ("-", sub),
+        ("*", mul),
+        ("/", div)
+    ]
+
+    for operator, operation in operations:
+        result = operation(a, b)
+        print(f"{a} {operator} {b} = {result}")
