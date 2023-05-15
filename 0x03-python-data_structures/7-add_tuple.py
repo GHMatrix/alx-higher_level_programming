765c62c490eb32dbfa78500165c9ade6c41d197b
GHMatrix <gsmensah@yahoo.com>
2023-05-15 01:23:36 -0700

    Adding two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
-    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
-    return tuple(map(sum, zip(tuple_a, tuple_b)))
+    """Add two tuples."""
+    tuple_a = tuple_a + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    tuple_b = tuple_b + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
