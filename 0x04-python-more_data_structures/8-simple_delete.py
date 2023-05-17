b20da3180be234b3c0c0d2eb1adf3fa9601d1920
GHMatrix <gsmensah@yahoo.com>
2023-05-17 08:51:52 -0700

    function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = a_dictionary.copy()
-    new_dict.pop(key, None)
-    return new_dict
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
