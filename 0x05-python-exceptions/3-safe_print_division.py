9914966bf39cedc733e9b7bb39e705059d3efed0
GHMatrix <gsmensah@yahoo.com>
2023-05-29 07:13:22 -0700

    divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {}".format(result))
-    except (TypeError, ZeroDivisionError) as e:
+    except ZeroDivisionError:
         result = None
+    finally:
         print("Inside result: {}".format(result))
-    return result
+        return result
