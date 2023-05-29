d4d0ed5576220919f931d2b53d5d72f2cf24f7ac
GHMatrix <gsmensah@yahoo.com>
2023-05-29 14:34:58 -0700

    Bytcode Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-    try:
-        result = sum(a ** b / i for i in range(1, 3) if i <= a)
-    except Exception:
-        result = b + a
-    return result
+def magic_calculation(a_param, b_param):
+    result_var = 0
+    for i_var in range(1, 3):
+        try:
+            if i_var > a_param:
+                raise Exception('Too far')
+            else:
+                result_var += a_param ** b_param / i_var
+        except Exception:
+            result_var = b_param + a_param
+            break
+    return result_var
