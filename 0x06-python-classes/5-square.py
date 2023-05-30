8aea16a80e77159480c9b143568af0de7d1ec0c1
GHMatrix <gsmensah@yahoo.com>
2023-05-30 11:31:48 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+Defining a class Square
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    This class defines a square by its size, a private instance attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieve the current size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Parameters:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Determines and return the current area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Print the square using the '#' character.
+
+        If size is equal to 0, prints an empty line.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
