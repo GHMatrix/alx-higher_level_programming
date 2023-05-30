1b55a9d135a1387eac19e98f91d43e65a56d799f
GHMatrix <gsmensah@yahoo.com>
2023-05-30 06:54:45 -0700

    an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This defines a class Square.
+"""
+
+
+class Square:
+    """
+    This represents a square.
+
+    This class is a blueprint for creating more objects of square.
+    """
+    pass
