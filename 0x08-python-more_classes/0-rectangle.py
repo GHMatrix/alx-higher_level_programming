74935b90b249399caabef47cfb649b2820f65885
GHMatrix <gsmensah@yahoo.com>
2023-06-05 03:06:57 -0700

    class that defines rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This defines a clas Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This represents a rectangle.
+
+    Blueprint for creating more objects of square.
+    """
+    pass
