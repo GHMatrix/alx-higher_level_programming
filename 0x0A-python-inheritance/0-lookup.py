0daae1f084a3dae0c49fbfc5528162d9d5088187
GHMatrix <gsmensah@yahoo.com>
2023-06-12 05:19:22 -0700

    A function that returns the list of available attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -14,13 +14,4 @@ def lookup(obj):
     Returns:
         A list containing the available attributes and methods of the object.
     """
-    attributes = []
-    methods = []
-
-    for item in dir(obj):
-        if not item.startswith('__') and not callable(getattr(obj, item)):
-            attributes.append(item)
-        elif callable(getattr(obj, item)):
-            methods.append(item)
-
-    return attributes + methods
+    return (dir(obj))
