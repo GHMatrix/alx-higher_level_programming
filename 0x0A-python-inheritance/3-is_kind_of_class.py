7313a5668673f158754833446d89e8cf45ac7a02
GHMatrix <gsmensah@yahoo.com>
2023-06-12 06:10:24 -0700

    function that returns True if the object is an instance of, or if the…

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,7 +8,8 @@ inherited from, the specified class
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if an object is an instance of, or if the object is an instance of a class that inherited from,
+    Check if an object is an instance of, or if the object is an
+    instance of a class that inherited from,
     the specified class.
 
     Args:
