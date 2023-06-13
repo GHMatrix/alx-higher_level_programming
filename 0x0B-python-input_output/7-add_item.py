c022620b4f804cbb57af99f8bcb7102e6ec80fdc
GHMatrix <gsmensah@yahoo.com>
2023-06-13 09:15:25 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,26 +7,23 @@ Defining script that adds all arguments
 from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""
+Adds command line arguments to a Python list and saves them to a JSON file.
 
+Command line arguments:
+The command line arguments provided when running the script.
 
-def main():
-    """
-    Adds command line arguments to a Python list and saves them to a JSON file.
+JSON file:
+The file to which the list will be saved in JSON format.
+"""
+filename = "add_item.json"
 
-    Command line arguments:
-        The command line arguments provided when running the script.
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-    JSON file:
-        The file to which the list will be saved in JSON format.
-    """
-    filename = "add_item.json"
+for arg in sys.argv[1:]:
+    json_list.append(arg)
 
-    try:
-        json_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        json_list = []
-
-    for arg in sys.argv[1:]:
-        json_list.append(arg)
-
-    save_to_json_file(json_list, filename)
+save_to_json_file(json_list, filename)
