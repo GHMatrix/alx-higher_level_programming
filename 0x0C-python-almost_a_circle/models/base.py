df438d02785993c7cea5b18908da195f7d8541c7
GHMatrix <gsmensah@yahoo.com>
2023-06-19 07:50:33 -0700

    20. JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -120,7 +120,7 @@ class Base:
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
-                csvfile.write("[]")
+                csvfile.write("")
             else:
                 if cls.__name__ == "Rectangle":
                     fieldnames = ["id", "width", "height", "x", "y"]
@@ -148,8 +148,8 @@ class Base:
                 else:
                     fieldnames = ["id", "size", "x", "y"]
                 reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([(k, int(v)) for k, v in dictionary.items(
-                    )]) for dictionary in reader]
+                next(reader)  # Skip the header row
+                list_dicts = [dict([(k, int(v)) for k, v in dictionary.items()]) for dictionary in reader]
                 return [cls.create(**dictionary) for dictionary in list_dicts]
         except IOError:
             return []
@@ -194,70 +194,3 @@ class Base:
             turt.hideturtle()
 
         turtle.exitonclick()
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Write the JSON string representation of list_objs to a file.
-
-        Args:
-            list_objs (list): A list of instances inheriting from Base.
-        """
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as jsonfile:
-            if list_objs is None:
-                jsonfile.write("[]")
-            else:
-                list_dicts = [obj.to_dictionary() for obj in list_objs]
-                json_string = cls.to_json_string(list_dicts)
-                jsonfile.write(json_string)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return the list represented by the JSON string.
-
-        Args:
-            json_string (str): A JSON string
-            representing a list of dictionaries.
-
-        Returns:
-            list: The list represented by the JSON string.
-        """
-        if json_string is None or json_string == "":
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Return an instance with attributes set according to the dictionary.
-
-        Args:
-            **dictionary (dict): Key/value pairs of attributes to initialize.
-
-        Returns:
-            object: An instance of the class with
-            attributes set according to the dictionary.
-        """
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new_instance = cls(1, 1)
-            else:
-                new_instance = cls(1)
-            new_instance.update(**dictionary)
-            return new_instance
-
-    @classmethod
-    def load_from_file(cls):
-        """Return a list of instances instantiated from a file of JSON strings.
-
-        Reads from `<cls.__name__>.json`.
-
-        Returns:
-            list: A list of instantiated classes.
-        """
-        filename = cls.__name__ + ".json"
-        try:
-            with open(filename, "r") as jsonfile:
-                list_dicts = cls.from_json_string(jsonfile.read())
-                return [cls.create(**dictionary) for dictionary in list_dicts]
-        except IOError:
-            return []
