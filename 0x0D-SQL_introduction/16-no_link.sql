ff1ddc777b73fabb21ab6dc647f81dfca9dc960b
GHMatrix <gsmensah@yahoo.com>
2023-07-18 08:18:39 -0700

    Listing records of table with name value

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Listing all records in the table second_table with name value
+-- Displaying the score and the name, ordered in descending order
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
