703fc33a364b8c522fe4559be8ad515481a0bbe6
GHMatrix <gsmensah@yahoo.com>
2023-07-18 07:45:33 -0700

    Listing records greater than or eqaul to 10 in desc order

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Listing all records from secon_table with score >= 10.
+-- Ordering records in descending order
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
