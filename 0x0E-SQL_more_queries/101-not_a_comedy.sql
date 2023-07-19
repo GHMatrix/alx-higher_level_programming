70d53d46fe79e267bc7c5059d93fb7c5ff1b56c0
GHMatrix <gsmensah@yahoo.com>
2023-07-19 08:45:08 -0700

    No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,4 @@
+-- Lisitng all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title
+-- Diplaying records in ascending order
+SELECT DISTINCT `title` FROM `tv_shows` AS t LEFT JOIN `tv_show_genres` AS s ON s.`show_id` = t.`id` LEFT JOIN `tv_genres` AS g ON g.`id` = s.`genre_id` WHERE t.`title` NOT IN (SELECT `title` FROM `tv_shows` AS t INNER JOIN `tv_show_genres` AS s ON s.`show_id` = t.`id` INNER JOIN `tv_genres` AS g ON g.`id` = s.`genre_id` WHERE g.`name` = "Comedy") ORDER BY `title`;
