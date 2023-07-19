d477280b867153a0a737398cc5f022d050e709e6
GHMatrix <gsmensah@yahoo.com>
2023-07-19 08:13:42 -0700

    Not my genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,3 @@
+-- Listing list all genres not linked to the show Dexter.
+-- Sorting results in ascending order
+SELECT DISTINCT `name` FROM `tv_genres` AS g INNER JOIN `tv_show_genres` AS s ON g.`id` = s.`genre_id` INNER JOIN `tv_shows` AS t ON s.`show_id` = t.`id` WHERE g.`name` NOT IN(SELECT `name` FROM `tv_genres` AS g INNER JOIN `tv_show_genres` AS ON g.`id` = s.`genre_id` INNER JOIN `tv_shows` AS t ON s.`show_id` = t.`id` WHERE t.`title` = "Dexter") ORDER BY g.`name`;
