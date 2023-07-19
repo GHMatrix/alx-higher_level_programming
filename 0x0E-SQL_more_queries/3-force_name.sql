3e0ac586ed4e93d902f629155ee6142b0ee24abd
GHMatrix <gsmensah@yahoo.com>
2023-07-19 04:37:02 -0700

    Creating table force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Creating a tble named force_name on MySQL server
+-- with id INT and name VARCHAR(256) can’t be null.
+CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
