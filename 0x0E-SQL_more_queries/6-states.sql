def7461f3755033b4c7441c64f11850428c5f363
GHMatrix <gsmensah@yahoo.com>
2023-07-19 05:52:21 -0700

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creating database named hbtn_0d_usa and table named states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (PRIMARY KEY(`id`), `id` INT	NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL);
