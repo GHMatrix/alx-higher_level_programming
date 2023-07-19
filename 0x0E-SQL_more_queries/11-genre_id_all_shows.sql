bf1d85a2567c724392184ea89c6d34b836114b56
GHMatrix <gsmensah@yahoo.com>
2023-07-19 06:54:52 -0700

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- Listing all shows in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Ordering results in order by tv_shows.title and tv_show_genres.genre_id
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
