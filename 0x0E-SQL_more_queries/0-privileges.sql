275e7de6432539b564bde1a3a301fa934803cdf6
GHMatrix <gsmensah@yahoo.com>
2023-07-19 05:55:54 -0700

    Listing all previleges of MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- Listing all previleges of MySQL users, user_0d_1 and user_0d_2 on server
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2') AND Host = 'localhost';
