bb41212d10e441263ad7de196fc6be39d49ad576
GHMatrix <gsmensah@yahoo.com>
2023-08-15 16:02:55 +0000

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const sortedDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  
+  if (sortedDict[occurrences] === undefined) {
+    sortedDict[occurrences] = [userId];
+  } else {
+    sortedDict[occurrences].push(userId);
+  }
+}
+
+console.log(sortedDict);
