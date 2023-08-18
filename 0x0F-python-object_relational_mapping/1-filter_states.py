3c7e608e18f5918d978e081cdb9d57d62e1a6c7d
GHMatrix <gsmensah@yahoo.com>
2023-08-18 06:16:34 -0700

    Filter States

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""List states with names starting with 'N' from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get MySQL connection parameters from command line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to select and display states starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
