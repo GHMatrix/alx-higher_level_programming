6eb99742721e487acc317547183a1d64acabddd6
GHMatrix <gsmensah@yahoo.com>
2023-08-19 04:19:47 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,14 @@
 Module to define the State class
 """
 
+import sys
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 # Create a base class for declarative models
 Base = declarative_base()
+
 
 class State(Base):
     """
@@ -19,18 +22,20 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
-    import sys
-    from sqlalchemy import create_engine
-    
     if len(sys.argv) != 4:
         print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
+    # Create the connection URL
+    connection_url =
+    f"mysql+mysqldb://{username}:{password}@localhost/{database}"
+
     # Connect to the MySQL server running on localhost at port 3306
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
+    engine = create_engine(connection_url, pool_pre_ping=True)
 
     # Create the 'states' table
     Base.metadata.create_all(engine)
