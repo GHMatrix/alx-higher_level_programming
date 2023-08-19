134cb346000774d6f215954323f410253a25e05c
GHMatrix <gsmensah@yahoo.com>
2023-08-19 06:20:53 -0700

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def update_state_name(username, password, database):
+    """Updates the state name"""
+    connection_url = f"mysql+mysqldb://{username}:{password}@localhost/{database}"
+    engine = create_engine(connection_url)
+    Session = sessionmaker(bind=engine)
+    
+    with Session() as session:
+        state = session.query(State).filter_by(id=2).first()
+        if state:
+            state.name = "New Mexico"
+            session.commit()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    update_state_name(username, password, database)
