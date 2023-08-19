fabe0078b6531084c555a61038af1614547f4886
GHMatrix <gsmensah@yahoo.com>
2023-08-19 06:14:14 -0700

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 
-""" Prints the State object with the name passed as an argument
-    from the database hbtn_0e_6_usa """
+"""
+Prints the State object with the name passed as an argument
+from the database hbtn_0e_6_usa
+"""
 
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    username, password, database,
-    state_name = argv[1], argv[2], argv[3], argv[4]
-
-    connection_url = "mysql+mysqldb://{}:{}@localhost/{}"
-    .format(username, password, database)
+def find_state_id(username, password, database, state_name):
+    """Finds and prints the state id"""
+    connection_url = f"mysql+mysqldb://{username}:{password}@localhost/{database}"
     engine = create_engine(connection_url)
     Session = sessionmaker(bind=engine)
-
+    
     with Session() as session:
         state = session.query(State).filter(State.name == state_name).first()
         if state:
             print(state.id)
         else:
             print("Not found")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    find_state_id(username, password, database, state_name)
