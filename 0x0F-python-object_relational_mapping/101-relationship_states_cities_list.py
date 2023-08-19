5959a9f0987828f826d20f6469e29148596113a0
GHMatrix <gsmensah@yahoo.com>
2023-08-19 08:50:22 -0700

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
 
 """
-Script to list all State objects and their corresponding City
-objects from the database hbtn_0e_101_usa
+Module that lists all State objects, and corresponding City objects,
 """
 
 from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State, City
 
-
-def list_states_and_cities(username, password, database):
-    """ List all State objects and their corresponding City objects """
-    connection_url = (
-            f"mysql+mysqldb://{username}:{password}@localhost/{database}"
+if __name__ == "__main__":
+    # Create an engine to connect to the database
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
+        pool_pre_ping=True
     )
-    engine = create_engine(connection_url, pool_pre_ping=True)
 
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_and_cities = session.query(State).order_by(State.id).all()
+    # Query all states with their cities
+    states = session.query(State).order_by(State.id).all()
 
-    for state in states_and_cities:
+    # Print states and cities
+    for state in states:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-    session.close()
-
-
-if __name__ == "__main__":
-    if len(argv) != 4:
-        print(f"Usage: {argv[0]} username password database")
-        exit(1)
-
-    list_states_and_cities(argv[1], argv[2], argv[3])
+            print(f"    {city.id}: {city.name}")
