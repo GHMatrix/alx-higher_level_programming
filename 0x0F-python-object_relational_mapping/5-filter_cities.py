71658a1ea6799657d80b6487ff2d03284a8c890c
GHMatrix <gsmensah@yahoo.com>
2023-08-19 02:40:19 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,25 @@
-#!/usr/bin/python3
-"""List cities of a specific state from the database hbtn_0e_4_usa"""
-
+#!/usr/bin/env python3
+"""Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time, write one
+that is safe from MySQL injections!
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Get command line arguments
     if len(sys.argv) != 5:
         print("Usage: {} username password database state_name"
               .format(sys.argv[0]))
         sys.exit(1)
 
+    # Get the arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server running on localhost at port 3306
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -27,22 +31,20 @@ if __name__ == "__main__":
     # Create a cursor object
     cursor = db.cursor()
 
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
+    # Execute the query with sanitized inputs
+    query = "SELECT cities.name FROM cities\
+             JOIN states ON cities.state_id = states.id\
+             WHERE states.name = %s\
+             ORDER BY cities.id ASC"
     cursor.execute(query, (state_name,))
 
-    # Fetch all rows
-    rows = cursor.fetchall()
+    # Fetch all the results
+    results = cursor.fetchall()
 
-    # Display results
-    for row in rows:
-        print(row[0])
+    # Display the results
+    city_names = [row[0] for row in results]
+    print(", ".join(city_names))
 
-    # Close the cursor and connection
+    # Close cursor and database connection
     cursor.close()
     db.close()
