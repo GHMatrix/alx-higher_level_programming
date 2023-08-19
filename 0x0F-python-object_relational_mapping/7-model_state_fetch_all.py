8608eebf86d396ff1da424ba9a546a34eccad825
GHMatrix <gsmensah@yahoo.com>
2023-08-19 04:46:28 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
 
-"""
-Script to list all State objects from the database hbtn_0e_6_usa
-"""
+"""Lists all State objects from the database hbtn_0e_6_usa"""
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Create the connection URL
+def main():
+    """Main function"""
+    username, password, database = argv[1], argv[2], argv[3]
     connection_url =
     f"mysql+mysqldb://{username}:{password}@localhost/{database}"
-
-    # Create the engine and establish the connection
     engine = create_engine(connection_url)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    # Retrieve and list all State objects in ascending order by states.id
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    with Session() as session:
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print(f"{state.id}: {state.name}")
 
-    # Close the session
-    session.close()
+
+if __name__ == "__main__":
+    main()
