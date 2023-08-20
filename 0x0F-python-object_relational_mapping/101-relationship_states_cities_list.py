71decc3c637633e9d9c747aacb8f1616b4cda98e
GHMatrix <gsmensah@yahoo.com>
2023-08-20 12:32:47 -0700

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,25 +7,19 @@ Module that lists all State objects, and corresponding City objects,
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Create an engine to connect to the database
     engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
-        pool_pre_ping=True
-    )
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all states with their cities
-    states = session.query(State).order_by(State.id).all()
-
-    # Print states and cities
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(f"    {city.id}: {city.name}")
+            print("\t{}: {}".format(city.id, city.name))
