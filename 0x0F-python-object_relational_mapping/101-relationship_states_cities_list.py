0d3879f5c098d09b169adbcedee9de1701cc5ea5
GHMatrix <gsmensah@yahoo.com>
2023-08-20 12:39:00 -0700

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-
-"""
-Module that lists all State objects, and corresponding City objects,
-"""
+""" Script that lists all State objects, and corresponding City objects,
+    contained in the database hbtn_0e_101_usa """
 
 from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+    username, password, db_name = argv[1], argv[2], argv[3]
 
+    # Create a connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    # Query the database using SQLAlchemy and retrieve the required data
+    result = session.query(State).order_by(State.id).all()
+
+    # Print the results in the desired format
+    for state in result:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
+
+    # Close the session
+    session.close()
