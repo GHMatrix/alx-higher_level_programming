83b30cf5312f02d6266252d0b2765ac4bd222d0b
GHMatrix <gsmensah@yahoo.com>
2023-08-31 15:59:15 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script hat makes a request to 0.0.0.0:5000/catch_me that causes the server to respond.
-curl -s 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
