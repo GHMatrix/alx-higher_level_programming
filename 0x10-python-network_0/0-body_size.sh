e526797ad7efa2ebc83c66ffab5c05c7cdb68432
GHMatrix <gsmensah@yahoo.com>
2023-08-31 05:04:37 -0700

    Bash script displaying the size of the body of response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-# Send a GET request and retrieve the response headers
-response_headers=$(curl -sI "$url")
-
-# Extract the Content-Length header value from the response headers
-content_length=$(echo "$response_headers" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-echo "$content_length"
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
