924440bc6cd438e312c922c50ef38e378ddde334
GHMatrix <gsmensah@yahoo.com>
2023-09-01 06:01:11 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,10 +12,13 @@ url = "https://alx-intranet.hbtn.io/status"
 
 try:
     response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
     content = response.text
 
     print("Body response:")
     print("    - type:", type(content))
     print("    - content:", content)
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
 except Exception as e:
-    print("An error occurred:", e)
+    print("An unexpected error occurred:", e)
