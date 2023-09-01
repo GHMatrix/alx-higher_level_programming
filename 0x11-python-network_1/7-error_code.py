0ff57273c745fcf63525eda00dc2c93f9f801323
GHMatrix <gsmensah@yahoo.com>
2023-09-01 06:57:44 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
 """
-This script sends a request to a given URL,
-displays the body of the response, and prints
-the error code if the HTTP status code is greater than or equal to 400.
+This script sends a request to a given URL and
+displays the body of the response (decoded in utf-8).
+It also handles HTTP errors and prints the
+HTTP status code in case of an error.
 
 Usage: ./7-error_code.py <URL>
 """
 
-import requests
+import urllib.request
+import urllib.error
 import sys
 
 
 def fetch_url_content(url):
     """
-    Sends a request to the provided URL, displays the body of the response, and
-    prints the error code if the HTTP status code
-    is greater than or equal to 400.
+    Sends a request to the provided URL and displays the body of the response
+    (decoded in utf-8). Handles HTTP errors and prints the
+    HTTP status code in case of an error.
 
     :param url: The URL to send the request to.
     """
     try:
-        response = requests.get(url)
-        content = response.text
-
-        print(content)
-
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(e)
 
 
 if __name__ == "__main__":
