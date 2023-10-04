3435cdf67564031576b5959a01ef3dfad56c10cf
GHMatrix <gsmensah@yahoo.com>
2023-10-04 12:22:20 +0000

    With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Wait for the document to be fully loaded before manipulating the DOM
+$(document).ready(function () {
+  // Select the <header> element using jQuery
+  $('header').css('color', '#FF0000');
+});
